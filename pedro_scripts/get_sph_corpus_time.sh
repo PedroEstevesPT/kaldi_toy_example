893dbba4dc1ef4fe175f409739d6ee908b5b5820
Pedro Fonseca Esteves <muradean@gmail.com>
2020-09-06 19:14:08 +0100

    removed ipynb checkpoints and added script to get the sum of the duration of all files in a dir of .sph files

---
+++ pedro_scripts/get_sph_corpus_time.sh
@@ -0,0 +1,24 @@
+# Pedro Fonseca Esteves 2020
+#
+# This script gets the duration of a corpus.
+#
+ln -s sph2pipe/sph2pipe tmp_sph2pipe
+tmp="${1}corpus_size_tmp/"
+
+if [ ! -d $tmp ]; then
+    mkdir $tmp;
+fi
+
+#Copies .sph files to temporary directory
+for filename in ${1}*.sph; do
+    cp $filename $tmp;
+done
+
+#Converts the files to wav
+for filename in ${tmp}*.sph; do
+    ./tmp_sph2pipe $filename -f wav ${filename%.*}.wav;
+    rm $filename
+done
+
+soxi -D $tmp/*.wav | awk '{s+=$1} END {print s " seconds"} '
+rm tmp_sph2pipe
